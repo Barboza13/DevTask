8000484090435eaa687c028d259a0785f999523f
Carlos Barboza <barbozacarlos678@gmail.com>
2024-12-31 19:51:33 -0300

    Se agrego contenido al HomeView.

--- resources/ts/views/HomeView.vue
+++ resources/ts/views/HomeView.vue
@@ -1,9 +1,66 @@
 <script lang="ts" setup>
+    import { ref, onMounted } from "vue"
     import MainLayout from "@layouts/MainLayout.vue"
+    import ProjectService from "@services/ProjectService"
+    import TaskService from "@services/TaskService"
+    import MemberService from "@services/MemberService"
+
+    import type { Ref } from "vue"
+
+    const projectService = new ProjectService()
+    const taskService = new TaskService()
+    const memberService = new MemberService()
+    const numberOfProjects: Ref<number> = ref(0)
+    const numberOfTasks: Ref<number> = ref(0)
+    const numberOfMembers: Ref<number> = ref(0)
+
+    onMounted(async () => {
+        await projectService.fetchProjects()
+        await taskService.fetchTasks()
+        await memberService.fetchMembers()
+
+        numberOfProjects.value = projectService.getProjects().length
+        numberOfTasks.value = taskService.getTasks().length
+        numberOfMembers.value = memberService.getMembers().length
+    })
 </script>
 
 <template>
-    <MainLayout />
+    <MainLayout>
+        <template v-slot:main>
+            <section
+                class="grid grid-cols-1 lg:grid-cols-3 place-content-start place-items-center w-full h-full p-4"
+            >
+                <div
+                    class="flex flex-col justify-center items-start w-3/4 h-30 bg-gray-200 rounded-md gap-4 p-4"
+                >
+                    <div class="flex justify-evenly items-center w-full gap-2">
+                        <v-icon name="md-foldercopy" fill="white" scale="3" />
+                        <h1 class="text-3xl">Proyectos</h1>
+                    </div>
+                    <h3>Registros: {{ numberOfProjects }}</h3>
+                </div>
+                <div
+                    class="flex flex-col justify-center items-start w-3/4 h-30 bg-gray-200 rounded-md gap-4 p-4"
+                >
+                    <div class="flex justify-evenly items-center w-full gap-2">
+                        <v-icon name="fa-file-code" fill="white" scale="3" />
+                        <h1 class="text-3xl">Tareas</h1>
+                    </div>
+                    <h3>Registros: {{ numberOfTasks }}</h3>
+                </div>
+                <div
+                    class="flex flex-col justify-center items-start w-3/4 h-30 bg-gray-200 rounded-md gap-4 p-4"
+                >
+                    <div class="flex justify-evenly items-center w-full gap-2">
+                        <v-icon name="io-person" fill="white" scale="3" />
+                        <h1 class="text-3xl">Miembros</h1>
+                    </div>
+                    <h3>Registros: {{ numberOfMembers }}</h3>
+                </div>
+            </section>
+        </template>
+    </MainLayout>
 </template>
 
 <style scoped></style>
